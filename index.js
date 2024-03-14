3350821cb1035272b0a826155989945e30285065
Tufailmahi <tufailmahi0331@gmail.com>
2024-03-14 07:09:40 +0500

    add new files

---
+++ index.js
@@ -0,0 +1,27 @@
+function rollDice() {
+    let diceImages = [
+        'Image/dice (1).png',
+        'Image/dice (2).png',
+        'Image/dice (3).png',
+        'Image/dice (4).png',
+        'Image/dice (5).png',
+        'Image/dice (6).png'
+    ];
+
+    var randomIndex = Math.floor(Math.random() * diceImages.length);
+    var randomDiceImage = diceImages[randomIndex];
+
+    document.getElementById('diceImage').src = randomDiceImage;
+}
+
+
+
+
+png
+
+
+
+
+
+
+
